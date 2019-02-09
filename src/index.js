1ca9ca8fc925baeebbfb4004113c63fe978ba2a7
Andreza Moreira <htmlandreza@gmail.com>
2019-02-09 18:22:57 -0200

    Criando a base da aplicação

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://admand:Marvin@03141718@cluster0-aryaf.mongodb.net/test?retryWrites=true', 
+    {
+        useNewUrlParser: true
+    }
+);
 
 app.get('/', (req, res) => {
     return res.send('Hello Word');
