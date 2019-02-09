da337e2d2fbc011268e039f5e0f5fe0559ff78f4
Andreza Moreira <htmlandreza@gmail.com>
2019-02-09 20:31:14 -0200

    Adicionando as ações no back-end

---
+++ src/controllers/TweetController.js
@@ -0,0 +1,18 @@
+const Tweet = require('../models/Tweet');
+
+module.exports = {
+    async index(req, res){
+        // listagem
+        // ordenar por data de criação com os mais novos me cima
+        const tweets = await Tweet.find({}).sort('-createdAt'); 
+
+        return res.json(tweets);
+    },
+
+    async store(req, res){
+        // criação de novos registros
+        const tweet = await Tweet.create(req.body);
+
+        return res.json(tweet);
+    }
+};
--- src/index.js
+++ src/index.js
@@ -3,15 +3,17 @@ const mongoose = require('mongoose');
 
 const app = express();
 
+const serve = require('http').Server(app);
+const io = require('socket.io')(serve);
+
 mongoose.connect('mongodb+srv://admand:Marvin@03141718@cluster0-aryaf.mongodb.net/test?retryWrites=true', 
     {
         useNewUrlParser: true
     }
 );
 
-app.get('/', (req, res) => {
-    return res.send('Hello Word');
-});
+app.use(express.json());
+app.use(require('./routes'));
 
 app.listen(3000, () => {
     console.log('Server started on port 3000');
